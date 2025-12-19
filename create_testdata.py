0a2fa0a92884ffbac1f8e643ea531995096f6122
Nobutake Kamiya <nobuta77to99@hotmail.com>
2025-12-19 16:59:28 +0100

    some changes

---
+++ create_testdata.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", required=True)
+    args = parser.parse_args()
+    filename = args.file
+    df = pd.read_excel(filename, header=0)
+    testdata_df = df[0:3000]
+    print("Erstelle Testdatensatz mit 3000 Einträgen...")
+    testdata_df.to_excel("dev_data/testdata.xlsx", index=False)
+
+if __name__ == "__main__":
+    main()
